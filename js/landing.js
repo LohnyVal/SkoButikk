f3febcdaed8d32965ff589e9f2cc52b861fc1b7c
LohnyVal <andreas.lohne.24@gmail.com>
2026-05-07 21:15:30 +0200

    Added navbar, and herosection

---
+++ js/landing.js
@@ -0,0 +1,14 @@
+let navgiationalButton = document.querySelectorAll(".links");
+let cartlink = document.getElementById("carticon");
+
+cartlink.addEventListener("click", () =>{
+    window.location.href = "../html/cart.html";
+})
+
+navgiationalButton.forEach((link) =>{
+    link.addEventListener("click", () =>{
+        
+    })
+})
+
+

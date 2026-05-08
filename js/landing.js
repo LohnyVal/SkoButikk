5a760deec2b6f1675b82f0a43e73144d0a8135a8
LohnyVal <andreas.lohne.24@gmail.com>
2026-05-08 15:09:17 +0200

    Almost finished with the home page

--- js/landing.js
+++ js/landing.js
@@ -1,6 +1,11 @@
 let navgiationalButton = document.querySelectorAll(".links");
 let linkdeceration = document.querySelectorAll(".linksdeceration");
 let cartlink = document.getElementById("carticon");
+let home = document.getElementById("logo");
+
+home.addEventListener("click", () =>[
+    window.location.href = "./index.html"
+])
 
 window.addEventListener("load", () =>{
     window.scrollTo({top:0})
